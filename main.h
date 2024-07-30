3d50dbbfa479a6c60f771a1d6c2a19c3590bb69e
VictorEne-Codes <omalevicene@gmail.com>
2024-07-30 12:17:54 -0700

    flags

--- main.h
+++ main.h
@@ -23,7 +23,7 @@ int printf_hex_helper(unsigned int n);
 int printf_input_reverse_str(va_list par);
 int find_strlen(char *c);
 int find_strlens(const char *c);
-
+int get_flags(const char *format, int *i);
 
 /**
  * struct format - a structure to match the conversion specifiers
